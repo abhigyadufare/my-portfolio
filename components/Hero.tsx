1534333a901f8673b7cb13468a02f714d3ba63b4
abhigya <abhigyadufare@gmail.com>
2025-03-09 23:19:14 +0530

    updated v4 styles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,9 +7,11 @@ const Hero = () => {
     <div className="pb-20 pt-36">
       <div>
         <Spotlight />
-        <h1 className="text-white text-4xl md:text-7xl outline-hidden">Hello World</h1>
+        <h1 className="text-white text-4xl font-bold text-center md:text-7xl outline-hidden">
+          Hello World
+        </h1>
       </div>
-      <div className=""></div>
+      <div className="container-2xl heading default-primary h-20 bg-amber-300 text-primary">yoohoo</div>
     </div>
   );
 };
