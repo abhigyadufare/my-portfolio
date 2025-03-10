f4e9cb97e85c186bef3dddfb084640e130d16f59
abhigya <abhigyadufare@gmail.com>
2025-03-10 23:56:37 +0530

    update grid

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
       <div className="container-2xl heading default-primary h-20 bg-amber-300 text-primary">
         yoohoo
       </div>
-      <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid bg-grid-black/[0.2] relative flex items-center justify-center">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
